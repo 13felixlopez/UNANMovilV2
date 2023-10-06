577c0f232f2c94a1448da81c7a0c1356a8b2e527
Dixon Cruz Blandon <drcblandon@gmail.com>
2023-10-06 15:52:15 -0600

    ADD:Vista de Restablecer Contraseña

--- UNANMovilV2/Vistas/Login.xaml.cs
+++ UNANMovilV2/Vistas/Login.xaml.cs
@@ -30,41 +30,47 @@ namespace UNANMovilV2.Vistas
 
         private async Task Logueo()
         {
-            if (string.IsNullOrEmpty(txtINSS.Text) || string.IsNullOrEmpty(txtPass.Text))
-            {
-                await DisplayAlert("ERROR", "Los campos estan vacios", "OK");
-            }
-            else
-            {
+            await Navigation.PushAsync(new Menu());
+            //if (string.IsNullOrEmpty(txtINSS.Text) || string.IsNullOrEmpty(txtPass.Text))
+            //{
+            //    await DisplayAlert("ERROR", "Los campos estan vacios", "OK");
+            //}
+            //else
+            //{
 
-                DataTable dt = new DataTable();
-                mprofes.INSS = int.Parse(txtINSS.Text);
-                mprofes.Password = Encrip.Encriptar(Encrip.Encriptar(txtPass.Text));
-                try
-                {
-                    dt = nprofes.Nprofe(mprofes);
-                    if (dt.Rows.Count > 0)
-                    {
-                        idprofesor = Convert.ToInt32(dt.Rows[0][0]);
-                        nombreprofe = dt.Rows[0][1].ToString();
-                        Icono = (byte[])dt.Rows[0][3];
-                        correo = dt.Rows[0][5].ToString();
-                        Tusuario = dt.Rows[0][6].ToString();
-                        INSS = int.Parse(dt.Rows[0][2].ToString());
-                        await Navigation.PushAsync(new Menu());
-                        txtINSS.Text = "";
-                        txtPass.Text = "";
-                    }
-                    else
-                    {
-                        await DisplayAlert("ERROR", "INSS o Contraseña Incorrectos", "Ok");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    await DisplayAlert("ERROR", ex.Message, "OK");
-                }
-            }
+            //    //DataTable dt = new DataTable();
+            //    //mprofes.INSS = int.Parse(txtINSS.Text);
+            //    //mprofes.Password = Encrip.Encriptar(Encrip.Encriptar(txtPass.Text));
+            //    try
+            //    {
+            //    //    dt = nprofes.Nprofe(mprofes);
+            //    //    if (dt.Rows.Count > 0)
+            //    //    {
+            //    //        idprofesor = Convert.ToInt32(dt.Rows[0][0]);
+            //    //        nombreprofe = dt.Rows[0][1].ToString();
+            //    //        Icono = (byte[])dt.Rows[0][3];
+            //    //        correo = dt.Rows[0][5].ToString();
+            //    //        Tusuario = dt.Rows[0][6].ToString();
+            //    //        INSS = int.Parse(dt.Rows[0][2].ToString());
+            //    //      await Navigation.PushAsync(new Menu());
+            //    //        txtINSS.Text = "";
+            //    //        txtPass.Text = "";
+            //    //    }
+            //        //else
+            //        //{
+            //        //    await DisplayAlert("ERROR", "INSS o Contraseña Incorrectos", "Ok");
+            //        //}
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        await DisplayAlert("ERROR", ex.Message, "OK");
+            //    }
+            //}
+        }
+
+        private async void BtnRestablecer_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RestablecerPass());
         }
     }
 }
